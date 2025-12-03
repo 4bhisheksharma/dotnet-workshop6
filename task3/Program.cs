4c4a73d4d32e50e962016e164add3c1b431e7505
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-12-03 18:57:58 +0545

    task3 completed

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        public static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+        {
+            foreach(int number in numbers)
+            {
+                if(condition(number))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello from task3");
+
+            int [] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55 };
+            Console.WriteLine("Even Numbers:");
+            ProcessNumbers(numbers, n => n % 2 == 0);
+
+            Console.WriteLine("\nNumbers greater than 10 :)");
+            ProcessNumbers(numbers, n => n > 10);
+
         }
     }
 }
