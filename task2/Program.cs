4c0b0b10c089eb5e59aa0a8c5e17371564d85df6
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-12-03 18:51:26 +0545

    task2.1 completed

--- task2/Program.cs
+++ task2/Program.cs
@@ -15,21 +15,31 @@ namespace task2
 
         //second question
         //***************************************
-        public delegate double DiscountDelegate(double price);
+        public delegate double DiscountStrategy(double price);
 
         //festival discount of 20% off
-        public static double FestivalDiscount(double price) => price * 0.20;
+        public static double FestivalDiscount(double price) => price * 0.80;
 
         //seasonal discount of 10% off
-        public static double SeasonalDiscount(double price) => price * 0.10;
+        public static double SeasonalDiscount(double price) => price * 0.90;
 
         //no discount season bina k ko discont?
         public static double NoDiscount(double price) => price;
 
 
+        //second point 1 question (this make me confused for a while)
+        //saying this second(point)1 cause why not?
+        //***************************************
+        public static double CalculateFinalPrice(double originalPrice, DiscountStrategy discountStrategy)
+        {
+            return discountStrategy(originalPrice);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from task2\n");
+
+            Console.WriteLine("Question 1");
 
             Calculate calculateAdd = Add;
             Calculate calculateSubtract = Subtract;
@@ -38,19 +48,22 @@ namespace task2
             Console.WriteLine($"Subtraction: {calculateSubtract(10, 5)}\n");
 
             Console.WriteLine("Question 2");
-            DiscountDelegate festivalDiscount = FestivalDiscount;
-            DiscountDelegate seasonalDiscount = SeasonalDiscount;
-            DiscountDelegate noDiscount = NoDiscount;
+            DiscountStrategy festivalDiscount = FestivalDiscount;
+            DiscountStrategy seasonalDiscount = SeasonalDiscount;
+            DiscountStrategy noDiscount = NoDiscount;
 
             double price = 1000.0;
 
             Console.WriteLine($"Original Amount: {price}");
             Console.WriteLine($"Festival Discounted Price: {festivalDiscount(price)}");
             Console.WriteLine($"Seasonal Discounted Price: {seasonalDiscount(price)}");
-            Console.WriteLine($"No Discount(original price): {noDiscount(price)}");
+            Console.WriteLine($"No Discount(original price): {noDiscount(price)}\n");
 
 
-
+            Console.WriteLine("Question 2.1");
+            //30% off using lamda expression
+            double lamdaPrice = CalculateFinalPrice(price, price => price * 0.70);
+            Console.WriteLine($"Final Price after 30% Discount: {lamdaPrice}");
         }
     }
 }
