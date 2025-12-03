2d64da3116fb738313eb96e06956f7bf6459f6a3
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-12-03 18:13:38 +0545

    task1 completed

---
+++ task1/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => length = value;
+        }
+
+        public double GetArea() => length * breadth;
+        public string GetDetails() => $"Length: {length}, Breadth: {breadth}";
+        public double GetPerimeter() => 2 * (length + breadth);
+    }
+}
